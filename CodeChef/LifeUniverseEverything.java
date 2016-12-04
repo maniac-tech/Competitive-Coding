0bff2d357a8b658caf561eda5dd2e9c2b081972a
Abhishek Jain <ajj_2396@yahoo.com>
2016-12-05 00:33:34 +0530

    Add Life, Universe and Everything

--- CodeChef/LifeUniverseEverything.java
+++ CodeChef/LifeUniverseEverything.java
@@ -1,20 +1,20 @@
-1.	import java.io.*;
-2.	class LifeUniverseEverything
-3.	{
-4.			public static void main (String[] args)throws IOException
-5.			{
-6.				BufferedReader obj = new BufferedReader (new InputStreamReader (System.in));
-7.				int a1 = 0;
-8.				
-9.				
-10.				while (true)
-11.				{
-12.				a1 = Integer.parseInt(obj.readLine());
-13.				
-14.				if(a1!=42)
-15.				System.out.println(a1);
-16.				else
-17.				break;
-18.				}
-19.			}
-20.	} 
+import java.io.*;
+class LifeUniverseEverything
+{
+	public static void main (String[] args)throws IOException
+	{
+		BufferedReader obj = new BufferedReader (new InputStreamReader (System.in));
+		int a1 = 0;
+		
+		
+		while (true)
+		{
+			a1 = Integer.parseInt(obj.readLine());
+			
+			if(a1!=42)
+				System.out.println(a1);
+			else
+				break;
+		}
+	}
+} 
