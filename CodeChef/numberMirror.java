47b59cdef56bf0fd71a9a4ea3214ce6c339d6bbc
Abhishek Jain <ajj_2396@yahoo.com>
2016-12-06 19:43:53 +0530

    Add NumberMirror

---
+++ CodeChef/numberMirror.java
@@ -0,0 +1,13 @@
+/*Problem Code: START01*/
+
+import java.io.*;
+/*public*/ class numberMirror
+{
+	public static void main(String [] args)throws IOException
+	{
+		BufferedReader obj = new BufferedReader (new InputStreamReader (System.in));
+
+		int num=Integer.parseInt(obj.readLine());
+		System.out.println(num);
+	}
+}
