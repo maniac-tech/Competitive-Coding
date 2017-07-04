b9f2a4bcee1ab5562f6abd6a8fc53cec477a3754
maniac-tech <ajj_2396@yahoo.com>
2017-07-04 21:28:35 +0530

    Added a trick to find out no of digits in a number

---
+++ tricks/noOfDigits.java
@@ -0,0 +1,9 @@
+// Log can be used to find the no of digits in a number.
+
+public class noofDigit{
+	public void main (){
+		int num = 300
+		int digit=(int)(Math.log10(num)+1); //num should be > 0
+		System.out.print(digit);
+	}
+}
